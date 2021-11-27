96a3ae74352abc92482acf2fdf67ccb8106a730d
Matthias Seitz <matthias.seitz@outlook.de>
2021-11-27 21:01:28 +0100

    fix: set dir explicitly

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    lalrpop::process_root().unwrap();
+    // lalrpop::process_root().unwrap();
+    let dir = format!("{}/src", std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    lalrpop::Configuration::new().process_dir(dir).unwrap();
 }
